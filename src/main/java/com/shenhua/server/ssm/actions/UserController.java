7a4ef6df875a071d217dcf7482ef7ac2a564d158
shenhuanet <shenhuanet@126.com>
2017-11-28 18:15:36 +0800

    Add mybaties

---
+++ src/main/java/com/shenhua/server/ssm/actions/UserController.java
@@ -0,0 +1,17 @@
+package com.shenhua.server.ssm.actions;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by shenhua on 2017-11-28-0028.
+ *
+ * @author shenhua
+ *         Email shenhuanet@126.com
+ */
+@Controller
+@RequestMapping("/admin")
+public class UserController {
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shenhua
+  Date: 2017-11-28-0028
+  Time: 01:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
